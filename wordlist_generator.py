d1da2d35da4f72cfcb4787116ff4b4e3d4cfb5b5
Andrew Jacobson <andrew.jacobson22@gmail.com>
2021-11-05 19:43:42 -0400

    Changing script to run via command line
    Adding parameterization functionality and ability to pass in phrases (in addition to single words). Not complete yet.

--- wordlist_generator.py
+++ wordlist_generator.py
@@ -5,20 +5,130 @@
 # 'coffee' become 'Coffee123', 'coff33!', etc.
 ########################################################################################################################
 
+import os
+import argparse
+
+# Function name: set_up_args
+# Parameters:
+# Returns:
+# Purpose:
+def set_up_args():
+
+    # Define the main parser for the argument list
+    parser_description = "A customizable wordlist/password generator. Takes an input file of either single" \
+                         "keywords ('coffee', 'drink', etc.) or phrases ('ILoveCoffee', 'DrinkCoffee', etc.)."
+    parser = argparse.ArgumentParser(description=parser_description)
+
+    # Add subparsers to the main parser
+    subparsers = parser.add_subparsers(dest="infile_type", help="Each of these options have additional arguments.")
+    words_parser = subparsers.add_parser("words", help="Specifies that the input file only contains single words.")
+    phrases_parser = subparsers.add_parser("phrases", help="Specified that the input file only contains phrases.")
+
+    # Add positional arguments to the "words" and "phrases" parsers
+    input_file_words = words_parser.add_argument("infile", help="Input file path and name.")
+    output_file_words = words_parser.add_argument("outfile", help="Output file path and name.")
+    input_file_phrases = phrases_parser.add_argument("infile", help="Input file path and name.")
+    output_file_phrases = phrases_parser.add_argument("outfile", help="Output file path and name.")
+
+    # Add subparsers to the "words" parser
+    # words_subparsers = words_parser.add_subparsers()
+    # Add a subparser for the "all" option
+    # all_options_words_parsers = words_subparsers.add_parser("xfm-all", help="Specifies that all transformations will be applied.")
+    # Add a subparser for the "some" option - this option has many optional flags
+    # some_options_words_parsers = words_subparsers.add_parser("xfm-some", help="Specifies that some transformations will be applied.")
+    # some_options_words_parsers.add_argument("-s", action="store_true", help="Apply character substitutions.")
+    # some_options_words_parsers.add_argument("-c", action="store_true", help="Apply character capitalizations.")
+    # some_options_words_parsers.add_argument("-n", action="store_true", help="Append numbers to the front.")
+    # some_options_words_parsers.add_argument("-u", action="store_true", help="Append numbers to the end.")
+    # some_options_words_parsers.add_argument("-y", action="store_true", help="Append symbols to the front.")
+    # some_options_words_parsers.add_argument("-m", action="store_true", help="Append symbols to the end.")
+
+    words_argument_group = words_parser.add_mutually_exclusive_group()
+    words_argument_group.add_argument("-b", "--basic", action="store_true", help="Apply basic transformations.")
+    words_argument_group.add_argument("-n", "--normal", action="store_true", help="Apply a standard/normal amount of transformations.")
+    words_argument_group.add_argument("-e", "--extreme", action="store_true", help="Apply every transformation.")
+
+    args = parser.parse_args()
+    print(args)
+    return args
+
+
 # Function name: valid_keyword
 # Parameters: Takes a string argument that is a single word/keyword
 # Returns: boolean True/False
 # Purpose: To run checks on the keywords that the user passes in to make sure they're valid
-def valid_keyword(keyword):
+def validate_input(keyword):
 
-    is_valid_keyword = True
+    is_valid_input = True
 
     # Verify that keyword only contains alpha characters
     if not keyword.isalpha():
+        is_valid_input = False
 
-        is_valid_keyword = False
+    if " " in keyword:
+        is_valid_input = False
 
-    return is_valid_keyword
+    return is_valid_input
+
+
+# Function name: create_appended_numbers_list
+# Parameters: Takes a string argument that is a single word/keyword
+# Returns: A list object
+# Purpose: To create a list of potential passwords with number combinations appended to the front and end of the
+# specified keyword.
+def create_symbols_appended_list(keyword, append_option):
+    # Define the list object to be returned
+    symbols_appended_list = []
+
+    # Define a list of common symbol combinations used in passwords
+    symbols_to_append = ['!', '!!', '!!!', '!@', '!@#']
+
+    # For each symbol combination in the previously defined list
+    for symbol in symbols_to_append:
+
+        # Append the symbols to the end of the keyword
+        if append_option == 'end' or append_option == 'both':
+            symbols_appended_list.append(keyword + symbol)
+
+        # Add the newly created password to the list to return
+        if append_option == 'front' or append_option == 'both':
+            symbols_appended_list.append(symbol + keyword)
+
+    return symbols_appended_list
+
+
+# Function name: create_appended_numbers_list
+# Parameters: Takes a string argument that is a single word/keyword
+# Returns: A list object
+# Purpose: To create a list of potential passwords with number combinations appended to the front and end of the
+# specified keyword.
+def create_numbers_appended_list(keyword, append_option):
+
+    # Define the list object to be returned
+    numbers_appended_list = []
+
+    # Define a list of common number combinations used in passwords
+    number_combinations = ['123', '234', '345', '456', '567', '678', '789', '890',
+                           '321', '432', '534', '654', '765', '876', '987', '098',
+                           '1234', '2345', '3456', '4567', '5678', '6789', '7890',
+                           '4321', '5432', '6543', '7654', '8765', '9876', '0987',
+                           '12345', '54321', '123456', '654321',
+                           '69', '420',
+                           '00', '000', '11', '111', '22', '222']
+
+    # Iterate through the number_combinations list and append the numbers to the keyword and add it to the list to
+    # return
+    for number in number_combinations:
+
+        # Append numbers at the end
+        if append_option == 'end' or append_option == 'both':
+            numbers_appended_list.append(keyword + number)
+
+        # Append numbers at the front
+        if append_option == 'front' or append_option == 'both':
+            numbers_appended_list.append(number + keyword)
+
+    return numbers_appended_list
 
 
 # Function name: created_substituted_list
@@ -80,41 +190,7 @@ def create_substituted_list(keyword):
 
                     keyword_substitutions_list.append(all_substituted_keyword)
 
-
     return keyword_substitutions_list
-
-
-# Function name: create_appended_numbers_list
-# Parameters: Takes a string argument that is a single word/keyword
-# Returns: A list object
-# Purpose: To create a list of potential passwords with number combinations appended to the front and end of the
-# specified keyword.
-def create_numbers_appended_list(keyword):
-
-    # Define the list object to be returned
-    numbers_appended_list = []
-
-    # Define a list of common number combinations used in passwords
-    number_combinations = ['123', '234', '345', '456', '567', '678', '789', '890',
-                           '321', '432', '534', '654', '765', '876', '987', '098',
-                           '1234', '2345', '3456', '4567', '5678', '6789', '7890',
-                           '4321', '5432', '6543', '7654', '8765', '9876', '0987',
-                           '12345', '54321', '123456', '654321',
-                           '69', '420',
-                           '00', '000', '11', '111', '22', '222']
-
-    # Iterate through the number_combinations list and append the numbers to the keyword and add it to the list to
-    # return
-    for number in number_combinations:
-
-        # Append numbers at the end
-        numbers_appended_list.append(keyword + number)
-
-        # Append numbers at the front
-        numbers_appended_list.append(number + keyword)
-
-
-    return numbers_appended_list
 
 
 # Function name: create_capitalized_list
@@ -147,31 +223,6 @@ def create_capitalized_list(keyword):
 
     return capitalized_list
 
-
-# Function name: create_appended_numbers_list
-# Parameters: Takes a string argument that is a single word/keyword
-# Returns: A list object
-# Purpose: To create a list of potential passwords with number combinations appended to the front and end of the
-# specified keyword.
-def create_symbols_appended_list(keyword):
-
-    # Define the list object to be returned
-    symbols_appended_list = []
-
-    # Define a list of common symbol combinations used in passwords
-    symbols_to_append = ['!', '!!', '!!!', '!@', '!@#']
-
-    # For each symbol combination in the previously defined list
-    for symbol in symbols_to_append:
-
-        # Append the symbols to the end of the keyword
-        new_keyword = keyword + symbol
-
-        # Add the newly created password to the list to return
-        symbols_appended_list.append(new_keyword)
-
-    return symbols_appended_list
-
 ########################################################################################################################
 ########################################################################################################################
 ########################################################################################################################
@@ -180,50 +231,101 @@ def create_symbols_appended_list(keyword):
 ########################################################################################################################
 ########################################################################################################################
 
-# Define the files used in the script - TODO: make these parameters you pass in upon script invocation
-file_keywords = open("keywords.txt", "r")
-file_passwords = open("passwords.txt", "w")
 
-# Variables used to keep track of the keywords processed and passwords generated. Used for output at the end.
-counter_keywords_processed = 0
+arguments = set_up_args()
+
+# Interpret the arguments provided
+
+# Define and open the input file
+try:
+    input_file = open(arguments.infile, "r")
+except FileNotFoundError as e:
+    print("Unable to open input file >>", e)
+    quit()
+# Define and open the output file
+try:
+    output_file = open(arguments.outfile, "w")
+except FileNotFoundError as e:
+    print("Unable to open output file >>", e)
+    quit()
+
+# Determine if processing a file with words or phrases
+is_words = False
+
+if arguments.infile_type == "words":
+
+    is_words = True
+
+    # Define settings based on options specified
+    words_transformation_settings = {'basic': {'substitutions': False,
+                                               'capitalization': False,
+                                               'numbers_append_front': False,
+                                               'numbers_append_end': True,
+                                               'symbols_append_front': False,
+                                               'symbols_append_end': True},
+                                    'normal': {'substitutions': True,
+                                               'capitalization': True,
+                                               'numbers_append_front': False,
+                                               'numbers_append_end': True,
+                                               'symbols_append_front': False,
+                                               'symbols_append_end': True},
+                                    'extreme': {'substitutions': True,
+                                               'capitalization': True,
+                                               'numbers_append_front': True,
+                                               'numbers_append_end': True,
+                                               'symbols_append_front': True,
+                                               'symbols_append_end': True}}
+    if arguments.basic is True:
+        words_transformation = 'basic'
+    elif arguments.normal is True:
+        words_transformation = 'normal'
+    elif arguments.extreme is True:
+        words_transformation = 'extreme'
+
+    selected_transformation_settings = words_transformation_settings[words_transformation]
+
+    print(selected_transformation_settings)
+
+# Variables used to keep track of the input processed and passwords generated. Used for output at the end.
+counter_processed = 0
 counter_passwords_generated = 0
 
-# Keywords from file list
-users_keywords = []
+# Variables to hold valid
+input_keywords = []
 
-# Keywords that are invalid
-users_invalid_keywords = []
+# Move the input file into its defined variable
+with input_file as reader:
 
-# Begin processing the keywords file
-with file_keywords as reader:
+    # For each word/phrase in the file
+    for line in reader.readlines():
 
-    # For each word in the file
-    for keyword in reader.readlines():
+        # Strip off the newline character
+        line = line.replace("\n", "")
 
-        # Strip off the newline character and make it lowercase (in case the user doesn't define the words correctly)
-        keyword = keyword.replace("\n", "").lower()
+        # Add the line to the list
+        input_keywords.append(line)
 
-        # Add the keyword to the list
-        users_keywords.append(keyword)
+# Close the input file
+input_file.close()
 
-# Close the keywords file
-file_keywords.close()
+# Variable to hold invalid input from the input file
+invalid_keywords = []
 
-# Validate users keywords - if they're good, continue. else, append them to a list of invalid keywords
-for keyword in users_keywords:
+# Validate input - if they're good, continue. else, append them to a list of invalid keywords
+for keyword in input_keywords:
 
-    if valid_keyword(keyword):
+    if validate_input(keyword):
         continue
     else:
-        users_invalid_keywords.append(keyword)
+        invalid_keywords.append(keyword)
 
 # For each invalid keyword, remove it from the list we want to process
-for keyword in users_invalid_keywords:
+for keyword in invalid_keywords:
 
-    users_keywords.remove(keyword)
+    input_keywords.remove(keyword)
 
-# For each keyword the user provided, process it
-for keyword in users_keywords:
+# For each keyword in the input file, process it
+for keyword in input_keywords:
 
     # Define the master keyword list - this gets reset for every keyword (don't want/need to hold EVERY keyword)
     master_keyword_list = []
@@ -417,24 +519,23 @@ for keyword in users_keywords:
     ################################################################################################################
 
     # Increment the counter for keywords processed
-    counter_keywords_processed += 1
+    counter_processed += 1
 
 # Now that all keywords have been created, write them to the passwords file
 for keyword in master_keyword_list:
 
     keyword = keyword + "\n"
-    file_passwords.write(keyword)
+    output_file.write(keyword)
 
     counter_passwords_generated += 1
 
-# Check if there were any keywords that weren't processed and display them
-if users_invalid_keywords:
+# Close the passwords file
+output_file.close()
 
-    print("Invalid keywords:", users_invalid_keywords)
+# Check if there were any keywords that weren't processed and display them
+if invalid_keywords:
+    print("Invalid keywords:", invalid_keywords)
 
 # Print some output to the user
-print("Keywords processed:", counter_keywords_processed)
+print("Keywords processed:", counter_processed)
 print(" Passwords created:", counter_passwords_generated)
-
-# Close the passwords file
-file_passwords.close()
